aea2b392aad29081c082fc75352b7875c3f0ad62
Juan Diego Mejia <58276675+diegojuan2104@users.noreply.github.com>
2021-05-09 21:13:23 -0500

    Login desing part 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/Login/Login"
 
 function App() {
   return (
-    <div >
+    <div className="app">
       <Login/>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,15 @@
 //rfce
 import React, { useState } from "react";
-import axiosClient from "../../config/axios"
-
+import axiosClient from "../../config/axios";
+import "./Login.css";
 
 function Login() {
   const [user, updateUser] = useState({
     email: "",
     password: "",
-    username: ""
+    username: "",
   });
+
   const { email, password, username } = user;
 
   const handleChange = (e) => {
@@ -18,41 +19,63 @@ function Login() {
     });
   };
 
+
+  const [login, changeLogin] = useState(false)
+
+  console.log(login);
+
+  const changeLoginAction = (status) => {
+    changeLogin(status)
+  }
+
+
   const onSubmitLogin = async (e) => {
     e.preventDefault();
-    const res = await axiosClient.post("/api/users/", user)
+    const res = await axiosClient.post("/api/users/", user);
     console.log(res);
-
-  }
+  };
   return (
-    <form onSubmit={onSubmitLogin}>
+    <div className="login">
+      <h1>My code Academy</h1>
+      <h2>For Developers</h2>
+      <p>Practice coding, prepare for interviews, and get hired.</p>
 
-    <input
-        type="text"
-        placeholder="username"
-        value={username}
-        onChange={handleChange}
-        name="username"
-      />
+      <div className="login__formOptions">
+          <button  className={login? "": "selected" } onClick={() => changeLogin(false)}>Sign up</button>
+          <button className={login? "selected" : "" } onClick={() => changeLogin(true)}>Log in</button>
+        </div>
+      <form>
+        
 
-      <input
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={handleChange}
-        name="email"
-      />
+        <div className="login__inputs">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleChange}
+            name="username"
+          />
 
-      <input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={handleChange}
-        name="password"
-      />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={handleChange}
+            name="email"
+          />
 
-      <button type="submit">Sign up</button>
-    </form>
+          <input
+            type="text"
+            placeholder="Your password"
+            value={password}
+            onChange={handleChange}
+            name="password"
+          />
+
+          <button type="submit" onClick={onSubmitLogin}>Create an account</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
