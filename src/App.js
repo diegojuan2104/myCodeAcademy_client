584f7a686ad9ca9f484d0676dddf16791161f77b
Juan Diego Mejia <58276675+diegojuan2104@users.noreply.github.com>
2021-05-12 23:04:44 -0500

    Login interface advance 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Auth from "./components/Auth/Auth.js"
-
+import "./App.css";
+import Auth from "./components/Auth/Auth.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Auth/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Auth} />
+        </Switch>
+      </Router>
     </div>
   );
 }
