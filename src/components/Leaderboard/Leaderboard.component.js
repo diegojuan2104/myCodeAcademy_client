800bc5834b1f7f8c4cf24d7a4754be679b22bb93
Juan Diego Mejia <58276675+diegojuan2104@users.noreply.github.com>
2021-05-26 17:05:35 -0500

    semantic

--- src/components/Leaderboard/Leaderboard.component.js
+++ src/components/Leaderboard/Leaderboard.component.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Header from '../Header/Header.component'
-import "./Leaderboard.styles.scss"
+import React from "react";
+import Header from "../Header/Header.component";
+import "./Leaderboard.styles.scss";
 
 
 function Leaderboard() {
-    return (
-        <div className="leaderboard">
-            <Header/>
-            <h1>Leaderboard</h1>
+  return (
+    <div>
+      <Header />
+      <div className="leaderboard">
+        <h2 className="leaderboard__title">Leaderboard</h2>
+        <div className="leaderboard__challenge-list">
+          <div className="leaderboard_user">
+              
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Leaderboard
+export default Leaderboard;
